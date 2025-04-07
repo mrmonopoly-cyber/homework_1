42da6218374e147cd015a07551f01a7fcde688b8
Tommaso Prandin <tommaso.prandin@raceup.it>
2025-04-07 11:49:28 +0200

    Aggiunto parsing input, conteggio punti e calcolo funzioni obiettivo

--- src/Homework_1.java
+++ src/Homework_1.java
@@ -4,194 +4,209 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.mllib.clustering.KMeans;
+import org.apache.spark.mllib.clustering.KMeansModel;
+import org.apache.spark.mllib.linalg.Vector;
+import org.apache.spark.mllib.linalg.Vectors;
 import scala.Tuple2;
+import scala.Tuple3;
 
-import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
 
 public class Homework_1 {
-
-    enum InputSet {
-        SetA,
-        SetB,
-    }
-
-    private static <T> void MRPrintStatistics(JavaRDD<Tuple2<InputSet,T>> universeSet, JavaRDD<T> centerSet) {
+    private static <T> void MRPrintStatistics(JavaRDD<Tuple2<InputSet, T>> universeSet, List<Vector> centerSet) {
         AtomicInteger centerIndex = new AtomicInteger(0);
-        centerSet.collect().forEach((c) -> {
-            JavaRDD<Tuple2<Long,Long>> belongCount = universeSet.map( t -> {
+        centerSet.forEach((c) -> {
+            JavaRDD<Tuple2<Long, Long>> belongCount = universeSet.map(t -> {
                 if (t._2.equals(c)) {
-                    switch (t._1)
-                    {
+                    switch (t._1) {
                         case SetA:
-                            return new Tuple2(1,0);
+                            return new Tuple2(1L, 0L);
                         case SetB:
-                            return new Tuple2(0,1);
+                            return new Tuple2(0L, 1L);
                     }
                 }
-                return new Tuple2(0,0);
+                return new Tuple2(0L, 0L);
             });
-            Tuple2<Long,Long> countResult = belongCount.reduce((a,b) ->{
+            Tuple2<Long, Long> countResult = belongCount.reduce((a, b) -> {
                 return new Tuple2(a._1 + b._1, a._2 + b._2);
             });
-            System.out.printf("i = %d, ",centerIndex.get());
+            System.out.printf("i = %d, ", centerIndex.get());
             System.out.printf("center = (%s), ", c.toString());
-            System.out.printf("NA%d = %d, ",centerIndex.get(), countResult._1);
-            System.out.printf("NB%d = %d\n",centerIndex.get(), countResult._2);
+            System.out.printf("NA%d = %d, ", centerIndex.get(), countResult._1);
+            System.out.printf("NB%d = %d\n", centerIndex.get(), countResult._2);
             centerIndex.addAndGet(1);
         });
 
     }
 
-    public static void main(String[] args) throws IOException {
+    private static double MRComputeStandardObjective(JavaRDD<Vector> points, List<Vector> centroids) {
+        Tuple2<Double, Integer> total = points
+                .mapPartitionsWithIndex((index, partition) -> {
+                    ArrayList<Tuple3<Integer, Double, Integer>> distances = new ArrayList<>();
+                    partition.forEachRemaining(point -> {
+                        // Compute min squared distance from centroids
+                        double cost = Double.POSITIVE_INFINITY;
+                        for (Vector center : centroids) {
+                            double distance = Vectors.sqdist(point, center);
+                            cost = Math.min(distance, cost);
+                        }
+                        distances.add(new Tuple3<>(index, cost, 1));
+                    });
+                    return distances.iterator();
+                }, true)
+                .groupBy(Tuple3::_1)
+                .mapToPair((partials) -> {
+                    Iterator<Tuple3<Integer, Double, Integer>> it = partials._2.iterator();
+                    double cost = 0.0;
+                    int count = 0;
+                    while (it.hasNext()) {
+                        Tuple3<Integer, Double, Integer> partial = it.next();
+                        cost += partial._2();
+                        count += partial._3();
+                    }
+                    return new Tuple2<>(1, new Tuple2<>(cost, count));
+                })
+                .reduceByKey((p1, p2) -> new Tuple2<>(p1._1 + p2._1, p1._2 + p2._2))
+                .collectAsMap()
+                .get(1);
+
+        return total._1 / total._2;
+
+    }
+
+    private static double MRComputeFairObjective(JavaPairRDD<InputSet, Vector> points, List<Vector> centroids) {
+        // First, separate points by InputSet (A and B)
+        JavaRDD<Vector> setAPoints = points
+                .filter(point -> point._1 == InputSet.SetA)
+                .map(point -> point._2);
+
+        JavaRDD<Vector> setBPoints = points
+                .filter(point -> point._1 == InputSet.SetB)
+                .map(point -> point._2);
+
+        // Compute objective for set A
+        double objA = MRComputeStandardObjective(setAPoints, centroids);
+
+        // Compute objective for set B
+        double objB = MRComputeStandardObjective(setBPoints, centroids);
+
+        // Return maximum of the two objectives (fair objective)
+        return Math.max(objA, objB);
+    }
+
+    public static void main(String[] args) {
 
         // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
         // CHECKING NUMBER OF CMD LINE PARAMETERS
         // Parameters are: num_partitions, <path_to_file>
         // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
 
-        if (args.length != 2) {
-            throw new IllegalArgumentException("USAGE: num_partitions file_path");
+        if (args.length != 4) {
+            throw new IllegalArgumentException("USAGE: num_partitions num_clusters max_iterations");
         }
 
         // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
         // SPARK SETUP
         // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
 
+        Logger rootLogger = Logger.getRootLogger();
+        rootLogger.setLevel(Level.OFF);
+
+        Logger.getLogger("org.apache.spark").setLevel(Level.ERROR);
+        Logger.getLogger("org.spark-project").setLevel(Level.ERROR);
         Logger.getLogger("org").setLevel(Level.OFF);
         Logger.getLogger("akka").setLevel(Level.OFF);
-        SparkConf conf = new SparkConf(true).setAppName("WordCount");
+        SparkConf conf = new SparkConf(true).setAppName("Homework1");
+        int K;
+        int M;
         JavaSparkContext sc = new JavaSparkContext(conf);
-        sc.setLogLevel("OFF");
+        sc.setLogLevel("ERROR");
 
         // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
         // INPUT READING
         // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
 
-        // Read number of partitions
-        int L = Integer.parseInt(args[0]);
+        // Read number of partitions and filename
+        String filename = args[0];
+        int L = Integer.parseInt(args[1]);
+        K = Integer.parseInt(args[2]);
+        M = Integer.parseInt(args[3]);
 
-        // Read input file and subdivide it into L random partitions
-        JavaRDD<String> docs = sc.textFile(args[1]).repartition(L).cache();
+        System.out.printf("Input file = %s, L = %d, K = %d, M = %d\n", filename, L, K, M);
+
+        // Read input file, parse content and subdivide it into L random partitions
+        JavaPairRDD<InputSet, Vector> inputPoints = sc.textFile(filename).mapToPair((line) -> {
+            ArrayList<Double> entries = new ArrayList<>();
+            InputSet set = InputSet.Unknown;
+            Iterator<String> tokens = Arrays.stream(line.split(",")).iterator();
+            while (tokens.hasNext()) {
+                String token = tokens.next();
+                if (tokens.hasNext()) {
+                    entries.add(Double.parseDouble(token));
+                } else {
+                    switch (token) {
+                        case "A":
+                            set = InputSet.SetA;
+                            break;
+                        case "B":
+                            set = InputSet.SetB;
+                            break;
+                        default:
+                            set = InputSet.Unknown;
+                    }
+                }
+            }
+            return new Tuple2<>(set, Vectors.dense(entries.stream().mapToDouble(Double::doubleValue).toArray()));
+        }).repartition(L).cache();
 
         // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-        // SETTING GLOBAL VARIABLES
+        // PRINT NUMBER OF POINTS
         // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
 
-        long numdocs, numwords;
-        numdocs = docs.count();
-        System.out.println("Number of documents = " + numdocs);
-        JavaPairRDD<String, Long> wordCounts;
-        Random randomGenerator = new Random();
+        // Count total points
+        long totalCount = inputPoints.count();
+        // Count points in Set A
+        long countA = inputPoints.filter(point -> point._1 == InputSet.SetA).count();
+        // Count points in Set B
+        long countB = inputPoints.filter(point -> point._1 == InputSet.SetB).count();
+        System.out.printf("N = %d, NA = %d, NB = %d\n", totalCount, countA, countB);
 
         // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-        // 1-ROUND WORD COUNT
+        // K-MEANS CLUSTERING
         // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
 
-        wordCounts = docs.flatMapToPair(myMethods::wordCountPerDoc) // <-- MAP PHASE (R1)
-                .reduceByKey((x, y) -> x+y);    // <-- REDUCE PHASE (R1)
-        numwords = wordCounts.count();
-        System.out.println("Number of distinct words in the documents = " + numwords);
+        // Strip class information from original dataset
+        JavaRDD<Vector> strippedInputPoints = inputPoints.mapPartitions((points) -> {
+            ArrayList<Vector> strippedPoints = new ArrayList<>();
+            while (points.hasNext()) {
+                strippedPoints.add(points.next()._2);
+            }
+            return strippedPoints.iterator();
+        }, true).cache();
 
-        // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-        // 2-ROUND WORD COUNT - RANDOM KEYS ASSIGNED IN MAP PHASE
-        // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+        // Cluster the data into two classes using KMeans
+        KMeansModel clusters = KMeans.train(strippedInputPoints.rdd(), K, M);
 
-        wordCounts = docs
-                .flatMapToPair((document) -> {    // <-- MAP PHASE (R1)
-                    String[] tokens = document.split(" ");
-                    HashMap<String, Long> counts = new HashMap<>();
-                    ArrayList<Tuple2<Integer, Tuple2<String, Long>>> pairs = new ArrayList<>();
-                    for (String token : tokens) {
-                        counts.put(token, 1L + counts.getOrDefault(token, 0L));
-                    }
-                    for (Map.Entry<String, Long> e : counts.entrySet()) {
-                        pairs.add(new Tuple2<>(randomGenerator.nextInt(L), new Tuple2<>(e.getKey(), e.getValue())));
-                    }
-                    return pairs.iterator();
-                })
-                .groupByKey()    // <-- REDUCE PHASE (R1)
-                .flatMapToPair(myMethods::gatherPairs) // <-- REDUCE PHASE (R1)
-                .reduceByKey((x, y) -> x+y); // <-- REDUCE PHASE (R2)
-        numwords = wordCounts.count();
-        System.out.println("Number of distinct words in the documents = " + numwords);
 
-        // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-        // 2-ROUND WORD COUNT - RANDOM KEYS ASSIGNED ON THE FLY
-        // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+        // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+        // STANDARD OBJECTIVE COST
+        // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+        double standard_cost = MRComputeStandardObjective(inputPoints.map(point -> point._2), Arrays.asList(clusters.clusterCenters()));
+        System.out.printf("Delta(U, C) = %f\n", standard_cost);
+        double fair_cost = MRComputeFairObjective(inputPoints, Arrays.asList(clusters.clusterCenters()));
+        System.out.printf("Phi(A, B, C) = %f\n", fair_cost);
 
-        wordCounts = docs.flatMapToPair(myMethods::wordCountPerDoc)
-                .groupBy((wordcountpair) -> randomGenerator.nextInt(L))  // <-- KEY ASSIGNMENT+SHFFLE+GROUPING
-                .flatMapToPair(myMethods::gatherPairs)
-                .reduceByKey((x, y) -> x+y); // <-- REDUCE PHASE (R2)
-        numwords = wordCounts.count();
-        System.out.println("Number of distinct words in the documents = " + numwords);
-
-        // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-        // 2-ROUND WORD COUNT - SPARK PARTITIONS
-        // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-
-        wordCounts = docs.flatMapToPair(myMethods::wordCountPerDoc)
-                .mapPartitionsToPair((element) -> {    // <-- REDUCE PHASE (R1)
-                    HashMap<String, Long> counts = new HashMap<>();
-                    while (element.hasNext()){
-                        Tuple2<String, Long> tuple = element.next();
-                        counts.put(tuple._1(), tuple._2() + counts.getOrDefault(tuple._1(), 0L));
-                    }
-                    ArrayList<Tuple2<String, Long>> pairs = new ArrayList<>();
-                    for (Map.Entry<String, Long> e : counts.entrySet()) {
-                        pairs.add(new Tuple2<>(e.getKey(), e.getValue()));
-                    }
-                    return pairs.iterator();
-                })
-                .groupByKey()     // <-- SHUFFLE+GROUPING
-                .mapValues((it) -> { // <-- REDUCE PHASE (R2)
-                    long sum = 0;
-                    for (long c : it) {
-                        sum += c;
-                    }
-                    return sum;
-                }); // Obs: one could use reduceByKey in place of groupByKey and mapValues
-        numwords = wordCounts.count();
-        System.out.println("Number of distinct words in the documents = " + numwords);
-
-        // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-        // COMPUTE AVERAGE WORD LENGTH
-        // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-
-        int avgwordlength = wordCounts
-                .map((tuple) -> tuple._1().length())
-                .reduce((x, y) -> x+y);
-        System.out.println("Average word length = " + (double) avgwordlength/ (double) numwords);
-
+//        MRPrintStatistics(inputPoints, Arrays.asList(clusters.clusterCenters()));
     }
 
-}
+    enum InputSet {
+        SetA, SetB, Unknown
+    }
 
-class myMethods {
-    public static Iterator<Tuple2<String,Long>> wordCountPerDoc(String document) {
-        String[] tokens = document.split(" ");
-        HashMap<String, Long> counts = new HashMap<>();
-        ArrayList<Tuple2<String, Long>> pairs = new ArrayList<>();
-        for (String token : tokens) {
-            counts.put(token, 1L + counts.getOrDefault(token, 0L));
-        }
-        for (Map.Entry<String, Long> e : counts.entrySet()) {
-            pairs.add(new Tuple2<>(e.getKey(), e.getValue()));
-        }
-        return pairs.iterator();
-    }
-    public static Iterator<Tuple2<String,Long>> gatherPairs(Tuple2<Integer,Iterable<Tuple2<String, Long>>> element) {
-        HashMap<String, Long> counts = new HashMap<>();
-        for (Tuple2<String, Long> c : element._2()) {
-            counts.put(c._1(), c._2() + counts.getOrDefault(c._1(), 0L));
-        }
-        ArrayList<Tuple2<String, Long>> pairs = new ArrayList<>();
-        for (Map.Entry<String, Long> e : counts.entrySet()) {
-            pairs.add(new Tuple2<>(e.getKey(), e.getValue()));
-        }
-        return pairs.iterator();
-    }
 }
